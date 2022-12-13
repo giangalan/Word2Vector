e60f8cad7e1bd6100ebd8e179ea8bbcbbf1da04d
giangalan <ganalanphan@gmail.com>
2022-12-13 17:45:22 +0700

    FromLab

--- py_scripts/word2vec_gensim.py
+++ py_scripts/word2vec_gensim.py
@@ -3,20 +3,20 @@ import gensim.downloader as api
 from gensim.test.utils import common_texts
 import pandas as pd
 
-# a = Word2Vec(sentences= common_texts, vector_size=100, window=5, min_count=1, workers=4)
-# a.save('w2v.model')
+a = Word2Vec(sentences=common_texts, vector_size=100, window=5, min_count=1, workers=4)
+a.save('w2v.model')
 #
-# b = Word2Vec.load('w2v.model')
+b = Word2Vec.load('w2v.model')
 # b.train([['hello', 'world']], total_examples=1, epochs=1)
 # vec = b.wv['computer']
 # sims = b.wv.most_similar('computer', topn=10)
-# vec_list = b.wv
-# vec_list.save('/home/code/NLP/Word_to_Vector/Vec/w2v_vectors.vector')
-# print(vec)
+vec_list = b.wv
+vec = vec_list.load('/home/code/NLP/Word2Vector/Vec/w2v_vectors.vector')
 
-wv = Word2Vec.load('word2vec-google-news-300')
 
-for i, word in enumerate(wv.index_to_key):
-    if i == 10:
-        break
-    print(f"word #{i}/{len(wv.index_to_key)} is {word}")
+# wv = Word2Vec.load('/home/code/NLP/Word2Vector/Vec/w2v_vectors.vector')
+#
+# for i, word in enumerate(wv.index_to_key):
+#     if i == 10:
+#         break
+#     print(f"word #{i}/{len(wv.index_to_key)} is {word}")
